cc551257384a0d4b0b30b7e3cde71fc7e4404018
imosudi2 <imosudi@outlook.com>
2025-06-27 08:34:08 +0100

    Code reviews

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ import os
 import requests
 from datetime import date
 from dotenv import load_dotenv
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+# reCAPTCHA keys
 
 app = Flask(__name__)
 #app.secret_key = os.urandom(24)
 print(os.environ.get('FLASK_SECRET_KEY'))
-app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'super-secret-key')
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')#, 'super-secret-key')
 # reCAPTCHA keys
 RECAPTCHA_SECRET_KEY = os.environ.get("RECAPTCHA_SECRET_KEY")
 RECAPTCHA_SITE_KEY = os.environ.get("RECAPTCHA_SITE_KEY")
